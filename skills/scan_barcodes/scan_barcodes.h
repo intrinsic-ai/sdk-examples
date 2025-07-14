54c94248474bf0561bf54998a10b8c528e26f823
Michael Carroll <mjcarroll@intrinsic.ai>
2025-07-14 20:48:13 +0000

    Update examples to 90d1f93ae301bc9d2d4fe1201668ec44b3eb782a
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- skills/scan_barcodes/scan_barcodes.h
+++ skills/scan_barcodes/scan_barcodes.h
@@ -1,17 +1,17 @@
-#ifndef SCAN_BARCODES_SCAN_BARCODES_H_
-#define SCAN_BARCODES_SCAN_BARCODES_H_
+#ifndef SDK_EXAMPLES_SKILLS_SCAN_BARCODES_SCAN_BARCODES_H_
+#define SDK_EXAMPLES_SKILLS_SCAN_BARCODES_SCAN_BARCODES_H_
 
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "absl/status/statusor.h"
-#include "intrinsic/perception/service/proto/camera_server.pb.h"
-#include "intrinsic/perception/service/proto/camera_server.grpc.pb.h"
+#include "intrinsic/perception/proto/v1/camera_service.grpc.pb.h"
+#include "intrinsic/perception/proto/v1/camera_service.pb.h"
 #include "intrinsic/skills/cc/skill_interface.h"
 #include "intrinsic/skills/proto/skill_service.pb.h"
 #include "opencv2/objdetect/barcode.hpp"
-
 #include "skills/scan_barcodes/scan_barcodes.pb.h"
 
 namespace scan_barcodes {
@@ -32,33 +32,29 @@ class ScanBarcodes : public intrinsic::skills::SkillInterface {
   // ---------------------------------------------------------------------------
 
   // Called once each time the skill is executed in a process.
-  absl::StatusOr<std::unique_ptr<google::protobuf::Message>>
-  Execute(const intrinsic::skills::ExecuteRequest& request,
-          intrinsic::skills::ExecuteContext& context) override;
+  absl::StatusOr<std::unique_ptr<google::protobuf::Message>> Execute(
+      const intrinsic::skills::ExecuteRequest& request,
+      intrinsic::skills::ExecuteContext& context) override;
 
  private:
-  absl::Status
-  ConnectToCamera(
-    const intrinsic_proto::resources::ResourceGrpcConnectionInfo& grpc_info,
-    const intrinsic_proto::perception::CameraConfig& camera_config,
-    std::unique_ptr<intrinsic_proto::perception::CameraServer::Stub>* camera_stub,
-    std::string* camera_handle);
+  absl::StatusOr<
+      std::unique_ptr<intrinsic_proto::perception::v1::CameraService::Stub>>
+  CreateCameraStub(
+      const intrinsic_proto::resources::ResourceGrpcConnectionInfo& grpc_info);
 
-  absl::StatusOr<intrinsic_proto::perception::CaptureResult>
-  CaptureImage(
-    const intrinsic_proto::resources::ResourceGrpcConnectionInfo& grpc_info,
-    intrinsic_proto::perception::CameraServer::Stub* camera_stub,
-    const std::string& camera_handle);
+  absl::StatusOr<intrinsic_proto::perception::v1::CaptureResult> Capture(
+      const intrinsic_proto::perception::v1::CameraConfig& camera_config,
+      const intrinsic_proto::resources::ResourceGrpcConnectionInfo& grpc_info,
+      intrinsic_proto::perception::v1::CameraService::Stub& camera_stub);
 
   absl::StatusOr<std::unique_ptr<::com::example::ScanBarcodesResult>>
-  ConvertToResultProto(
-    const std::vector<std::string>& decoded_data,
-    const std::vector<std::string>& decoded_types,
-    const std::vector<cv::Point2f>& detected_corners);
+  ConvertToResultProto(const std::vector<std::string>& decoded_data,
+                       const std::vector<std::string>& decoded_types,
+                       const std::vector<cv::Point2f>& detected_corners);
 
   cv::barcode::BarcodeDetector detector_;
 };
 
 }  // namespace scan_barcodes
 
-#endif  // SCAN_BARCODES_SCAN_BARCODES_H_
+#endif  // SDK_EXAMPLES_SKILLS_SCAN_BARCODES_SCAN_BARCODES_H_
